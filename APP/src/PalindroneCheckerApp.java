4a807020884da77d182f77f55bc4bad16d4629aa
mayanklr76-cyber <mayanklr76@gmail.com>
2026-03-17 15:33:04 +0530

    UC2

--- APP/src/PalindroneCheckerApp.java
+++ APP/src/PalindroneCheckerApp.java
@@ -1,5 +1,24 @@
+// UseCase2PalindromeCheckerApp.java
+
 public class PalindroneCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker App Management System");
+
+        // Hardcoded string
+        String str = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+
+        // Check if palindrome using if-else
+        if (str.equals(reversed)) {
+            System.out.println("The string \"" + str + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + str + "\" is NOT a Palindrome.");
+        }
     }
 }
