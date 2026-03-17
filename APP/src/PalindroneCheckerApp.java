f4324ca7abe0f96f2257956a478e622eb2a86f3d
mayanklr76-cyber <mayanklr76@gmail.com>
2026-03-17 15:01:20 +0530

    UC1

--- APP/src/PalindroneCheckerApp.java
+++ APP/src/PalindroneCheckerApp.java
@@ -1,5 +1,5 @@
 public class PalindroneCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrone Checker App Management System");
+        System.out.println("Welcome to the Palindrome Checker App Management System");
     }
 }
